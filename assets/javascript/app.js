37614a3c006ef669bd353ed2aa6c2f1be7b30073
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-21 18:44:36 -0400

    html and style done

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBQE3NvNl8Ey_CCRRoq6v8QpSCtcNLMR-Y",
+  authDomain: "train-scheduler-600d7.firebaseapp.com",
+  databaseURL: "https://train-scheduler-600d7.firebaseio.com",
+  projectId: "train-scheduler-600d7",
+  storageBucket: "",
+  messagingSenderId: "656090633042"
+};
+firebase.initializeApp(config);
