734b794a58d9489ed0c4c20db12e49c10384f1e9
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-22 15:36:23 -0400

    jquery link being wonky began logic for train input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,57 @@ var config = {
   authDomain: "train-scheduler-600d7.firebaseapp.com",
   databaseURL: "https://train-scheduler-600d7.firebaseio.com",
   projectId: "train-scheduler-600d7",
-  storageBucket: "",
+  storageBucket: "train-scheduler-600d7.appspot.com",
   messagingSenderId: "656090633042"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// 2. Button for adding trains
+$("document").ready(function () {
+
+  // event.preventDefault();
+
+  var trainObj;
+
+  retreiveInputs = function () {
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var circle = $("#circle-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
+      name: trainName,
+      circle: circle,
+      frequency: frequency,
+      first: firstTrain
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.circle);
+    console.log(newTrain.frequency);
+    console.log(newTrain.first);
+
+    clearInput();
+
+    alert("New Train Successfully added... to hell!")
+
+  };
+
+
+  clearInput = function () {
+    $("#train-name-input").val("");
+    $("#circle-input").val("");
+    $("#frequency-input").val("");
+    $("#first-train-input").val("");
+  }
+
+});
+// $("document").ready(function () {
+
+// });
